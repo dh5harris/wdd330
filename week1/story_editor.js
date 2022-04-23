9c909a4cb945e656c4bf6ab16e8c032f83e644ee
dh5harris <dh5harris@hotmail.com>
2022-04-22 21:59:52 -0500

    fixed an error in displayStory()

---
+++ week1/story_editor.js
@@ -0,0 +1,18 @@
+function loadStory() {
+  let storyName = document.getElementById('name_input').value;
+  let storyHTML = localStorage.getItem(storyName);
+  document.getElementById('story_editor').value = storyHTML;
+}
+
+function saveStory() {
+  let storyName = document.getElementById('name_input').value;
+  let storyHTML =  document.getElementById('story_editor').value;
+  localStorage.setItem(storyName, storyHTML);
+}
+
+function displayStory() {
+  let storyName = document.getElementById('name_input').value;
+  let storyHTML = localStorage.getItem(storyName);
+  document.getElementById('story_display').innerHTML = storyHTML;
+
+}
