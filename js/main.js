8c9bad43dee378413013c2a79e6867bd6e99d973
dh5harris <dh5harris@hotmail.com>
2022-05-29 00:35:28 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 const links = [
   {
-    label: "Week1 projects/notes ",
+    label: "Week 1 projects/notes ",
     url: 'week1/index.html'
   },
   {
-    label: "Week2 projects/notes ",
+    label: "Week 2 projects/notes ",
     url: 'week2/index.html'
   },
   {
-    label: "Week3 projects/notes ",
+    label: "Week 3 projects/notes ",
     url: 'week3/index.html'
   },
   {
-    label: "Week4 projects/notes ",
+    label: "Week 4 projects/notes ",
     url: 'week4/index.html'
   },
   {
-    label: "Week5 projects/notes ",
+    label: "Week 5 projects/notes ",
     url: 'week5/index.html'
   },
   {
-    label: "Week6: Todo App",
-    url: 'week6/index.html'
+    label: "Week 6: Todo App",
+    url: 'todoList/index.html'
   }
 ]
 
