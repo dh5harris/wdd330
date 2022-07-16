0f25dacf853acb348af976d6c951f3203d6d9c0e
dh5harris <dh5harris@hotmail.com>
2022-07-16 13:39:52 -0500

    removed unused code

--- memory-game/js/memory.js
+++ memory-game/js/memory.js
@@ -1,30 +1,17 @@
 
 const requestURL = 'https://dh5harris.github.io/wdd330/memory-game/data/data.json';
 const game = document.getElementById('memory-game');
-// let best = document.getElementById('best');
+
 let moves = 0
-// let bestScore = localStorage.getItem('score');
-// console.log(bestScore);
-
-// function getScore(key) {
-//   bestScore = localStorage.getItem(key);
-// }
-
-// function addScore(key, value) {
-//   localStorage.setItem(key, value)
-// }
 
 
 function fetchObject(url) {
   fetch(url)
   .then(response => response.json())
   .then(data => {
-    // displayCard(game, images)
     displayCard(game, getImages(data));
     addCardListener();
   })
-  // getScore('score');
-  
 }
 
 function displayCard(parent, items) {
@@ -41,6 +28,7 @@ function makeCard(objItem) {
 }
 
 function getImages(objItem) {
+  // Add to copies of the images and then randomly sort them
   const images = [...objItem['images'],...objItem['images']];
   images.sort(() => Math.random() - 0.5);
   return images
@@ -48,12 +36,8 @@ function getImages(objItem) {
 
 function checkMatch() {
   const turnedOverCards = document.querySelectorAll('.turned-over');
-  // moves++;
-  console.log(turnedOverCards);
   if (turnedOverCards.length === 2) {
-    // displayBest();
     displayTurns(moves);
-    // getTurns(turnedOverCards.length % 2);
     if (turnedOverCards[0].children[0].name === turnedOverCards[1].children[0].name) {
       
       displayMessage('That is a match!')
@@ -90,7 +74,6 @@ function checkWin() {
   if (flippedCards.length === 16) {
     checkScore();
     displayMessage('You found all the matches!');
-
   }
 }
 
@@ -108,24 +91,5 @@ function displayTurns(number) {
   let turns = document.getElementById('turns')
   turns.textContent = number / 2;
 }
-
-// function displayBest() {
-//   let best = document.getElementById('best');
-//   if (bestScore === null) {
-//     best.textContent = 0;
-//   } else {
-//     best.textContent = bestScore;
-//   }
-// }
-
-
-
-// function checkScore()  {
-  // if (bestScore <= localStorage.getItem('score') || bestScore === null) {
-  //   addScore('score', moves);
-  // };
-//   addScore('score', moves);
-// }
-
 
 fetchObject(requestURL);
